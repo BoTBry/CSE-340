389f9baaf7dfa3e37e8c3d8a3f2da2109e4f7ae5
BoTBry <abchigbu@gmail.com>
2025-07-26 23:17:02 +0100

    week02 done

---
+++ render.com.session.sql
@@ -0,0 +1,15 @@
+INSERT INTO public.assignment2 (first_name, last_name, age, review)
+VALUES(
+    'Beckham',
+    'Obiudo',
+    18,
+    'Latom🙏',
+    '1 Star'
+),
+(
+    'Secretariat',
+    'Njoku',
+    19,
+    'Hate it, The Chelsea was tasteless😠',
+    '5 Stars'
+);
