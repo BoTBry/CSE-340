dda588168f90b4efc33e7a560e18591e35f2b8c4
BoTBry <abchigbu@gmail.com>
2026-09-06 21:42:47 +0100

    ejs prac

---
+++ test.js
@@ -0,0 +1,13 @@
+function fullname(p) {
+    let name = "Chigbu";
+    if (p === name) {
+        let fullname = `${name} Chimela.`;
+        console.log(fullname);
+    }
+}
+
+fullname("Chigbu");
+
+const greet = ()=>{
+
+}
