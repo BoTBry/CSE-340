60f07861ad2a0ebac614d87e19ec897567416d66
BoTBry <abchigbu@gmail.com>
2026-09-14 00:11:07 +0100

    done

--- src/models/db.js
+++ src/models/db.js
@@ -13,10 +13,10 @@ import { Pool } from 'pg';
  */
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: process.env.NODE_ENV === "production"
-        ? { rejectUnauthorized: false }
-        : false
-    // ssl: true
+    // ssl: process.env.NODE_ENV === "production"
+    //     ? { rejectUnauthorized: false }
+    //     : false
+    ssl: true
 });
 
 /**
