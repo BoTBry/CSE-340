ad956c1e4ac568410267974e0192ab37293272df
BoTBry <abchigbu@gmail.com>
2026-09-26 17:31:30 +0100

    Week03 Done

--- src/controller/categories.js
+++ src/controller/categories.js
@@ -1,5 +1,5 @@
 // Import any needed model functions
-import { getAllCategories, getCategoryById, getCategoriesByServiceProjectId, getServiceProjectsByCategoryId, getServiceProjectByProjectId } from '../models/categories.js';
+import { getAllCategories, getCategoryById, getServiceProjectsByCategoryId, getServiceProjectByProjectId } from '../models/categories.js';
 
 const showCategoriesPage = async (req, res) =>{
     const categories = await getAllCategories();
@@ -12,14 +12,12 @@ const showCategoriesPage = async (req, res) =>{
 const showCategoryDetailsPage = async (req, res) => {
     const categoryId = req.params.id;
     const categoryDetails = await getCategoryById(categoryId);
-    const categoryService = await getCategoriesByServiceProjectId(categoryId);
     const projectCategory = await getServiceProjectsByCategoryId(categoryId);
     console.log(categoryDetails);
     console.log(projectCategory);
-    console.log(categoryService);
 
     const title = categoryDetails.name;
-    res.render('Category', { title, categoryDetails, categoryService, projectCategory });
+    res.render('Category', { title, categoryDetails, projectCategory });
 }
 
 const showProjectDetailsPageById = async (req, res) => {
